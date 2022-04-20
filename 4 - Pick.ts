ba68b181964f355f562977420244df9db803152a
Osman Rafi <33856265+Osman-Rafi@users.noreply.github.com>
2022-04-20 23:43:56 +0600

    solution: 4 - Pick

---
+++ 4 - Pick.ts
@@ -0,0 +1,15 @@
+type MyPick<T, K extends keyof T> = { [P in K]: T[P] };
+
+type Todo = {
+  title: string;
+  description: string;
+  completed: boolean;
+};
+
+type TodoPreview = MyPick<Todo, "title" | "description">;
+
+const todo: TodoPreview = {
+  title: "Doing some TS",
+  description: "solving TS challenges",
+};
+const aaa = 10;
