8a93bd2b0c46380bdc2debe06e6203df26998d32
Osman Rafi <33856265+Osman-Rafi@users.noreply.github.com>
2022-04-20 23:53:21 +0600

    fix: bug fix

--- 4-Pick.ts
+++ 4-Pick.ts
@@ -1,3 +1,8 @@
+/* 
+-> Problem statement
+-> https://github.com/type-challenges/type-challenges/blob/main/questions/00004-easy-pick/README.md
+*/
+
 type MyPick<T, K extends keyof T> = { [P in K]: T[P] };
 
 type Todo = {
